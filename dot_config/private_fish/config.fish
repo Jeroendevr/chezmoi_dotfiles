90ec9ecf563629cda9b959900418269751201dde
Jeroen de Vries <hayer@MacBook-Pro-van-Jeroen.fritz.box>
2022-11-24 09:09:31 +0100

    added fish plugins

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+string match -q "$TERM_PROGRAM" "vscode"
+and . (code --locate-shell-integration-path fish)
