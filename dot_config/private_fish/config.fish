0538351979537a809c6769cfc03cc5c80d2f562d
Jeroen de Vries <jeroen.vries@stillfront.com>
2023-07-04 13:19:30 +0200

    fish config template

--- dot_config/private_fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-string match -q "$TERM_PROGRAM" "vscode"
-and . (code --locate-shell-integration-path fish)
-
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-pyenv init - | source
