b124a36125ea6471160d622d2e3e9846fab28165
Jeroen de Vries <jeroen.vries@stillfront.com>
2023-07-04 13:26:59 +0200

    set variables.fish

---
+++ dot_config/private_fish/variables.fish
@@ -0,0 +1,5 @@
+set fisher_path "$__fish_config_dir/plugins"
+
+# Tide : change here instead of `tide configure`
+set --universal tide_character_bg_color normal
+set --universal tide_character_color 5FD700
